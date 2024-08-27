d4596e1319673188c4302aff03e0d25ecaa3aef5
Gates <gustavorpsantos1999@gmail.com>
2024-08-27 17:21:51 -0300

    glance feito

--- android/app/src/main/kotlin/com/example/home_widgets_flutter/CounterWidget.kt
+++ android/app/src/main/kotlin/com/example/home_widgets_flutter/CounterWidget.kt
@@ -4,23 +4,20 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import com.example.home_widgets_flutter.HomeWidgetProvider
 
 /**
  * Implementation of App Widget functionality.
  */
 class CounterWidget : AppWidgetProvider() {
-
-    val counter = 0;
-
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
@@ -32,33 +29,6 @@ class CounterWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-    internal fun incrementCounter(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-        val widgetText = context.getString(R.string.appwidget_text)
-        // Construct the RemoteViews object
-        val views = RemoteViews(context.packageName, R.layout.counter_widget)
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
-    internal fun decrementCounter(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-        val widgetText = context.getString(R.string.appwidget_text)
-        // Construct the RemoteViews object
-        val views = RemoteViews(context.packageName, R.layout.counter_widget)
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
-
 }
 
 internal fun updateAppWidget(
---
+++ android/app/src/main/kotlin/com/example/home_widgets_flutter/counterReciver.kt
@@ -0,0 +1,7 @@
+package com.example.home_widgets_flutter.glance
+
+import HomeWidgetGlanceWidgetReceiver
+
+class HomeWidgetReceiver : HomeWidgetGlanceWidgetReceiver<HomeWidgetGlanceAppWidget>() {
+    override val glanceAppWidget = HomeWidgetGlanceAppWidget()
+}
